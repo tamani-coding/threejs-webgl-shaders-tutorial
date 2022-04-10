11a825fa80d9041d79f57f48177f8f4587dcd511
genka <tamani.coding@gmail.com>
2022-04-10 17:58:53 +0200

    refactore shaders

--- src/utils/shaders.ts
+++ src/utils/shaders.ts
@@ -1,27 +1,5 @@
-const layerStruct = `
-    struct Layer {
-        sampler2D colorMap;
-        vec2 flowDirection;
-        float flowSpeed;
-        vec2 repeat;
-    };
-`;
-
-const offsetUV = `
-    vec2 offsetUV(vec2 uv, vec2 direction, float flowSpeed, vec2 repeat) {
-        vec2 flowDirection = normalize(direction);
-        return vec2( 
-            uv.x * repeat.x - flowDirection.x * flowSpeed * time, 
-            uv.y * repeat.y - flowDirection.y * flowSpeed * time
-        );
-    }
-`;
-
 export const vertexShader = `
-
-
     precision mediump float;
-    precision mediump int;
     precision mediump int;
 
     uniform mat4 modelViewMatrix;
@@ -39,41 +17,50 @@ export const vertexShader = `
 
         vUv = uv;
     }
-
-    
 `;
-
 
 export const fragmentShader = `
     precision mediump float;
     precision mediump int;
 
-    `
-    + layerStruct + 
-    `
+    struct Layer {
+        sampler2D colorMap;
+        vec2 flowDirection;
+        float flowSpeed;
+        vec2 repeat;
+    };
 
     uniform Layer textureLayer01;
     uniform Layer textureLayer02;
     uniform float time;
 
+    vec2 offsetUV(vec2 uv, vec2 direction, float flowSpeed, vec2 repeat) {
+        vec2 flowDirection = normalize(direction);
+        return vec2(
+            uv.x * repeat.x - flowDirection.x * flowSpeed * time,
+            uv.y * repeat.y - flowDirection.y * flowSpeed * time
+        );
+    }
+
     varying vec2 vUv;
 
-    `
-
-    +  offsetUV +
-    
-    `
-
     void main()	{
-        // UV with flow direction, speed and time
-        vec2 flow01 = offsetUV(vUv, textureLayer01.flowDirection, textureLayer01.flowSpeed, textureLayer01.repeat);
-        vec2 flow02 = offsetUV(vUv, textureLayer02.flowDirection, textureLayer02.flowSpeed, textureLayer02.repeat);
+        vec2 flow01 = offsetUV(
+            vUv,
+            textureLayer01.flowDirection,
+            textureLayer01.flowSpeed,
+            textureLayer01.repeat
+        );
+        vec2 flow02 = offsetUV(
+            vUv,
+            textureLayer02.flowDirection,
+            textureLayer02.flowSpeed,
+            textureLayer02.repeat
+        );
 
-        // color maps
         vec4 layer01 = texture2D(textureLayer01.colorMap, flow01);
         vec4 layer02 = texture2D(textureLayer02.colorMap, flow02);
 
-        // result fragment color
         gl_FragColor = layer01 + layer02;
     }
 `;
